f2677981d032e0e6ed40fa5f531e0616b6c99531
vinay vishwakarma <vnypro98@gmail.com>
2024-06-22 10:06:09 +0100

    modified few code

--- forLoop.js
+++ forLoop.js
@@ -1,8 +1,19 @@
-function forLoop(num){
-    let x='';
-    for (let index = 0; index <num; index++) {
-        x+='*'
+// function forLoop(num){
+//     let x='';
+//     for (let index = 0; index <num; index++) {
+//         x+='*'
+//     }
+//     console.log(x);
+// }
+// }
+// forLoop(5)
+
+
+function forLoop(num) {
+    const arr = [];
+    for (let index = 0; index < num; index++) {
+        arr.push('*');
     }
-    console.log(x);
+    console.log(arr.join(''));
 }
 forLoop(5)
