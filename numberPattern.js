79d8d547c3c4e180149f4b75b19adb7a3341853e
Vinay <vnypro98@gmail.com>
2024-09-16 22:51:39 +0530

    added few more programs

---
+++ numberPattern.js
@@ -0,0 +1,16 @@
+// for (let i = 1; i <= 5; i++) {
+//     let line = '';
+//     for (let j = 1; j <= i; j++) {
+//         line += j;
+//     }
+//     console.log(line);
+// }
+
+
+for (let i = 1; i <= 5; i++) {
+    let line = '';
+    for (let j = 1; j <= i; j++) {
+        line += j + ' ';
+    }
+    console.log(line.trim()); 
+}
