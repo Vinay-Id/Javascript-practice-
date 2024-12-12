ba512dab3892097448680afed09ffdb84b7a6e3a
vinay vishwakarma <vnypro98@gmail.com>
2024-12-12 18:10:41 +0530

    added few more program

---
+++ compareTwoStr.js
@@ -0,0 +1,13 @@
+function compareStrings(str1, str2) {
+    // Compare the strings
+    if (str1 === str2) {
+        console.log("The strings are equal.");
+    } else {
+        console.log("The strings are not equal.");
+    }
+}
+
+let string1 = "Hello, World!";
+let string2 = "Hello, world!"; 
+
+compareStrings(string1, string2);
